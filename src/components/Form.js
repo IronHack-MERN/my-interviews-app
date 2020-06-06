d627beed1628ec54fff34ebf2feff57e0d1fc281
Elena Piaggio <elenapiaggio@gmail.com>
2020-06-06 20:31:36 +0200

    ADD interview state

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,44 +1,77 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 
 const Form = () => {
+  // Create state of apply
+  const [interview, updateInterview] = useState({
+    company: "",
+    recruiter: "",
+    date: "",
+    time: "",
+    technicalTest: "",
+  });
+
+  const updateState = (e) => {
+    console.log(`Writing in ${e.target.name}`);
+    updateInterview({
+      ...interview,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  // Extract input values
+  const { company, recruiter, date, time, technicalTest } = interview;
+
   return (
     <Fragment>
       <h2>Create </h2>
+
       <form>
-        <labe>Name</labe>
+        <label>Name</label>
         <input
           type="text"
           name="company"
+          value={company}
           className="u-full-width"
           placeholder="Compony name"
+          onChange={updateState}
         />
-        <labe>Recruiter</labe>
+
+        <label>Recruiter</label>
         <input
           type="text"
           name="recruiter"
+          value={recruiter}
           className="u-full-width"
           placeholder="Recruiter name"
+          onChange={updateState}
         />
-        <labe>Date</labe>
-        <input type="date" 
-          name="date" 
-          className="u-full-width" />
-        <labe>Time</labe>
+
+        <label>Date</label>
+        <input
+          type="date"
+          name="date"
+          value={date}
+          className="u-full-width"
+          onChange={updateState}
+        />
+
+        <label>Time</label>
         <input
           type="time"
           name="time"
+          value={time}
           className="u-full-width"
+          onChange={updateState}
         />
+
         <label>Technical Test</label>
         <textarea
-          className='u-full-width'
-          name='technical-test'
-        >
-        </textarea>
-        <button
-          type='submit'
-          className='u-full-width button-primary'
-        >
+          className="u-full-width"
+          name="technicalTest"
+          value={technicalTest}
+          onChange={updateState}
+        ></textarea>
+        <button type="submit" className="u-full-width button-primary">
           ADD apply
         </button>
       </form>
